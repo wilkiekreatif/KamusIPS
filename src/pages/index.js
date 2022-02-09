80558546155d38ab37585e03ca98b090dc572141
Wildan Auliana <wilkie.creative23@gmail.com>
2022-02-09 09:19:03 +0700

    instalasi @react-navigation/native

---
+++ src/pages/index.js
@@ -0,0 +1,72 @@
+import Splash from './Splash';
+import Mainmenu from './Mainmenu';
+// import Kelas1 from './Kelas1';
+// import Kelas2 from './Kelas2';
+// import Kelas3 from './Kelas3';
+// import ByAbjad from './ByAbjad';
+// import BySearch from './BySearch';
+// import Detail from './Detail';
+// import HurufA from './Detail/HurufA';
+// import HurufB from './Detail/HurufB';
+// import HurufC from './Detail/HurufC';
+// import HurufD from './Detail/HurufD';
+// import HurufE from './Detail/HurufE';
+// import HurufF from './Detail/HurufF';
+// import HurufG from './Detail/HurufG';
+// import HurufH from './Detail/HurufH';
+// import HurufI from './Detail/HurufI';
+// import HurufJ from './Detail/HurufJ';
+// import HurufK from './Detail/HurufK';
+// import HurufL from './Detail/HurufL';
+// import HurufM from './Detail/HurufM';
+// import HurufN from './Detail/HurufN';
+// import HurufO from './Detail/HurufO';
+// import HurufP from './Detail/HurufP';
+// import HurufQ from './Detail/HurufQ';
+// import HurufR from './Detail/HurufR';
+// import HurufS from './Detail/HurufS';
+// import HurufT from './Detail/HurufT';
+// import HurufU from './Detail/HurufU';
+// import HurufV from './Detail/HurufV';
+// import HurufW from './Detail/HurufW';
+// import HurufX from './Detail/HurufX';
+// import HurufY from './Detail/HurufY';
+// import HurufZ from './Detail/HurufZ';
+// import Tentang from './Tentang';
+export {
+  Splash,
+  Mainmenu,
+  // Kelas1,
+  // Kelas2,
+  // Kelas3,
+  // ByAbjad,
+  // BySearch,
+  // Detail,
+  // HurufA,
+  // HurufB,
+  // HurufC,
+  // HurufD,
+  // HurufE,
+  // HurufF,
+  // HurufG,
+  // HurufH,
+  // HurufI,
+  // HurufJ,
+  // HurufK,
+  // HurufL,
+  // HurufM,
+  // HurufN,
+  // HurufO,
+  // HurufP,
+  // HurufQ,
+  // HurufR,
+  // HurufS,
+  // HurufT,
+  // HurufU,
+  // HurufV,
+  // HurufW,
+  // HurufX,
+  // HurufY,
+  // HurufZ,
+  // Tentang,
+};
